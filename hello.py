7e218593b9ad731feae9ed7d930956ecff7a1af9
YNUPanpan <632344940@qq.com>
2025-06-04 14:11:02 +0800

    first

---
+++ hello.py
@@ -0,0 +1,11 @@
+#初始化
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+ return '<h1>Hello, {}!</h1>'.format(name)
